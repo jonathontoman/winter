d0f44f889aad37fe4f8124ee0f9a277f566041e4
jonathon <jonathontoman@hotmail.com>
2016-04-24 15:40:56 +0100

    dynamic svg graph started

--- js/winter.js
+++ js/winter.js
@@ -1,14 +1,30 @@
 window.addEventListener('load', function () {
+    /* load node data via ajax */
     $.ajax({
-        method:'GET',
+        method: 'GET',
         url: 'data/nodes.json',
         dataType: "json",
         success: function (data) {
-            console.log(data);
+            displayNodes(data);
         },
-        error: function (xhr,ajaxOptions,error){            
+        error: function (xhr, ajaxOptions, error) {
             console.log(ajaxOptions);
             console.log(error);
         }
-    });
+    });            
 });
+
+function displayNodes(data)
+{
+    
+/*    var nodes = data.nodes;
+    console.log(nodes);
+    
+    var length = nodes.length;    
+    for (var i = 0; i < length ; i++)
+    {
+        $('#graph').append('<circle cx="10%" cy="20%" r="2%" stroke="black" stroke-width="3" fill="red"/>');
+        console.log('display nodes method');
+    }*/
+    
+}
