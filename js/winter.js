61abaca47d87c953b125104faa390084d8a34146
jonathon <jonathontoman@hotmail.com>
2016-04-24 16:11:01 +0100

    dynamic svg graph tidying up

--- js/winter.js
+++ js/winter.js
@@ -6,25 +6,33 @@ window.addEventListener('load', function () {
         dataType: "json",
         success: function (data) {
             displayNodes(data);
+
+            $('#body').append('<h2>adasdasdasdsa<h2/>')
         },
         error: function (xhr, ajaxOptions, error) {
             console.log(ajaxOptions);
             console.log(error);
         }
-    });            
+    });
 });
 
-function displayNodes(data)
-{
-    
-/*    var nodes = data.nodes;
+function displayNodes(data) {
+
+    var nodes = data.nodes;
     console.log(nodes);
-    
-    var length = nodes.length;    
-    for (var i = 0; i < length ; i++)
-    {
-        $('#graph').append('<circle cx="10%" cy="20%" r="2%" stroke="black" stroke-width="3" fill="red"/>');
-        console.log('display nodes method');
-    }*/
-    
+    var length = nodes.length;
+
+    // create new html elements 
+    for (var i = 0; i < length; i++) {
+        var circle = document.createElementNS("http://www.w3.org/2000/svg", 'circle');
+        circle.setAttribute('cx', nodes[i].x + '0%');
+        circle.setAttribute('cy', nodes[i].y+ '0%');
+        circle.setAttribute('r', '2%');
+        circle.setAttribute('fill', 'red');
+        circle.setAttribute('stroke', 'red');
+        circle.setAttribute('stroke-width', '1');
+        document.getElementById('networkGraph').appendChild(circle);
+        console.log(nodes[i].x + "," + nodes[i].y);
+    }
+
 }
