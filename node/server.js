b6ec48440cd543f91ff2555ce155f53d49207354
jonathon <jonathontoman@hotmail.com>
2016-05-05 23:14:58 +0100

    started graph data structures

--- node/server.js
+++ node/server.js
@@ -1,26 +1,8 @@
 /*
 Start building a basic RESTful HTTP server using Express
 */
-var XMLPath = "../xml/larosiere.xml";
 var express = require('express');
 var app = express();
-
-function loadXMLDoc(filePath) {
-    var fs = require('fs');
-    var xml2js = require('xml2js');
-    var json;
-    try {
-        var fileData = fs.readFileSync(filePath, 'ascii');
-        var parser = new xml2js.Parser();
-        parser.parseString(fileData.substring(0, fileData.length), function (err, result) {
-            json = result.osm
-        });
-        return json;
-    } catch (ex) {
-        console.log(ex)
-    }
-    return json;
-};
 
 /*
 The route of our API 
